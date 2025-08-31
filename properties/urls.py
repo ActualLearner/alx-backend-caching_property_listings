a29e650d84e6cb0a7c8aa3db6f0c052c949264d1
ActualLearner <abdellahshafi7@gmail.com>
2025-08-31 22:34:30 +0300

    add cache property list view

---
+++ properties/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # The empty path '' corresponds to the root of the path defined in the project's urls.py
+    # So this will handle requests to /properties/
+    path("", views.property_list, name="property-list"),
+]
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+from .models import Property
+
+
+# This decorator will cache the entire response of this view for 15 minutes.
+@cache_page(60 * 15)
+def property_list(request):
+    """
+    A view that retrieves all properties from the database
+    and returns them as a JSON list. The response is cached.
+    """
+    # Get all property objects from the database
+    properties = Property.objects.all()
+
+    # Convert the queryset into a list of dictionaries to be serialized into JSON.
+    # .values() is efficient as it only selects the fields we need.
+    data = list(properties.values("title", "description", "price", "location"))
+
+    # Return the data as a JSON response.
+    # safe=False is required to allow a list to be the top-level object.
+    return JsonResponse(data, safe=False)
